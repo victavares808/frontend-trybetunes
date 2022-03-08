159e65b9205e6df199f65d7f26c25b99b850a739
vicdev <vtavares.tur@gmail.com>
2022-03-08 19:10:25 -0300

    rq2 finalizado

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header data-testid="header-component">TrybeTunes</header>
+    );
+  }
+}
+
+export default Header;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Album extends Component {
   render() {
     return (
+      <>
+        <Header />
         <div data-testid="page-album">Album</div>
+      </>
     );
   }
 }
