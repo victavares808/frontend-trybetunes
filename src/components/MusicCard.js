9364543eee445ba338f57b6b69c5d31b6832719f
vicdev <vtavares.tur@gmail.com>
2022-03-15 10:39:02 -0300

    requisito 7

---
+++ src/components/MusicCard.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+/* Sobre <audio> <track> </audio>
+ ref->https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/track
+Sobre <code> ref -> https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/code
+ */
+class MusicCard extends Component {
+  render() {
+    const { trackInfo, previewtrack } = this.props;
+    return (
+      <div>
+        <h4>
+          {
+            trackInfo
+          }
+        </h4>
+        <audio data-testid="audio-component" src={ previewtrack } controls>
+          <track kind="captions" />
+          Your chosen track
+        </audio>
+        <code>song</code>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackInfo: PropTypes.string.isRequired,
+  previewtrack: PropTypes.string.isRequired,
+};
+
+export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,15 +1,78 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      artist: '',
+      album: '',
+      musics: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMusic();
+  }
+
+  fetchMusic = () => {
+    this.setState(
+      { loading: true },
+      async () => {
+        const { match } = this.props;
+        const { id } = match.params;
+        const answer = await getMusics(id);
+        this.setState({
+          loading: false,
+          musics: answer.filter((music, index) => index !== 0),
+          artist: answer[0].artistName,
+          album: answer[0].collectionName,
+          albumArtwork: answer[0].artworkUrl100,
+        });
+      },
+    );
+  }
+
   render() {
+    const { musics, artist, album, albumArtwork, loading } = this.state;
+    const showLoadingMessage = <p>Carregando...</p>;
     return (
       <>
         <Header />
         <div data-testid="page-album">Album</div>
+        {loading ? (
+          showLoadingMessage
+        ) : (
+          <div>
+            <h2 data-testid="album-name">{album}</h2>
+            <img src={ albumArtwork } alt="" />
+            <h3 data-testid="artist-name">{artist}</h3>
+            {musics.map((song) => (
+              <MusicCard
+                key={ song.trackId }
+                music={ musics }
+                trackInfo={ song.trackName }
+                artist={ song.artistName }
+              />
+            ))}
+          </div>
+        )}
       </>
     );
   }
 }
-
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }),
+};
+Album.defaultProps = {
+  match: {},
+};
 export default Album;
